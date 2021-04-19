697fcee7116239f748462ddc8aa8f74aaac27ac9
wangya <wangya@021.com>
2021-04-19 20:23:16 +0800

    优化

--- gxnews/src/components/PageIndex.vue
+++ gxnews/src/components/PageIndex.vue
@@ -9,43 +9,53 @@
                 </ul>
             </div>
         </div>
-        <div class="n-title-logo" @click="reloadHome()">
-            <img src=".././assets/logo.png"/>
-        </div>
-        <div class="n-left">
-            <ul class="n-left-ul">
-                <li v-for="item in titleList" 
-                    :key="item.id"
-                    @click="gotoCategry(item.cateId)">
-                    <a target="_self" href='javascript:' :class="[item.id == selectIndex ? 'active' : 'inactive']">{{item.cateName}}</a></li>
-            </ul>
+        <div class="n_left">
+            <div class="n_left_inner">
+                <div class="n_title_logo" @click="reloadHome()">
+                    <img src=".././assets/logo.png"/>
+                </div>
+                <ul class="n-left-ul">
+                    <li v-for="item in titleList" 
+                        :key="item.id"
+                        @click="gotoCategry(item.cateId)">
+                        <a target="_self" href='javascript:' :class="[item.id == selectIndex ? 'active' : 'inactive']">{{item.cateName}}</a></li>
+                </ul>
+            </div>
         </div>
         <div class="n-middle">
             <div class="n-content">
                 <div v-show="showHomeFlag" class="n-content-item" v-for="item in newsList" :key="item.id">
-                    <a class="n_right_link" href="javascript:"><h3 @click="gotoNews(item.id)">{{item.title}}</h3></a>
-                    <div class="n-content-item-list" @click="gotoNews(item.id)">
-                        <img v-for="index in item.pics" :key="index" :src='index'/>
-                        <div class="n-conten-show-more">查看详情></div>
+                    <div class="n_content_image" @click="gotoNews(item.id)">
+                        <a target="_blank" href="javascript:"><img :src='item.pics[0]'/></a>
                     </div>
-                    <p @click="gotoNews(item.id)">{{item.from}}&nbsp;&nbsp;{{item.time}}</p>
+                    <div class="n_content_desc">
+                        <div class="n_content_desc_inner" @click="gotoNews(item.id)">
+                            <h2><a target="_blank" href="javascript:">{{item.title}}</a></h2>
+                            <p class="n_content_info">
+                                <a target="_self" href="javascript:">{{titleList[selectIndex].cateName}}</a>&nbsp;
+                                <a target="_self" href="javascript:">{{item.from}}</a>&nbsp;
+                                <span>{{item.time}}</span>
+                            </p>
+                        </div>
+                    </div>
                 </div>
                 <!-- <PageTemp ref="pageTemp" v-show="!showHomeFlag"></PageTemp> -->
             </div>
-            <div class="n-right">
+            <div class="n_right">
                 <ImageSlider></ImageSlider>
-                <ul class="n-right-list">
-                    <h3>24小时排行榜</h3>
-                    <li v-for="item in twelveList" :key="item.id">
-                        <div id="pic">
-                            <img :src="item.pics[0]" @click="gotoNews(item.id)"/>
-                        </div>
-                        <div style="n-right-list-news-title" @click="gotoNews(item.id)">
-                            <a class="n_right_link" href="javascript:">{{item.title}}</a>
-                            <div class="n-right-list-news-author" @click="gotoNews(item.id)">{{item.from}}&nbsp;&nbsp;&nbsp;{{item.time}}</div>
-                        </div>
-                    </li>
-                </ul>
+                <div class="n_right_container">
+                    <div class="n_right_today">今日热点</div>
+                    <ul class="n_right_list">
+                        <li v-for="item in twelveList" :key="item.id">
+                            <a class="image" href="javascript:" :title="item.title">
+                                <img :src="item.pics[0]" @click="gotoNews(item.id)">
+                            </a>
+                            <p>
+                                <a href="javascript:" :title="item.title">{{item.title}}</a>
+                            </p>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
@@ -90,14 +100,14 @@ export default {
             if(res.code != 200) return;
             _this.twelveList = res.data;
         })
-        window.onscroll = this.listScroll;
-        this.listScroll();
+        // window.onscroll = this.listScroll;
+        // this.listScroll();
     },
     methods:{
         listScroll(evt){
-            let div = document.getElementsByClassName("n-left")[0];
+            let div = document.getElementsByClassName("n_left")[0];
             let titlediv = document.getElementsByClassName("n-main")[0];
-            let logo = document.getElementsByClassName("n-title-logo")[0];
+            let logo = document.getElementsByClassName("n_title_logo")[0];
             let left = titlediv.offsetLeft;
             let num = parseInt(left) + 50;
             div.style.left = num + "px";
@@ -138,12 +148,26 @@ export default {
     }
     body {
         background: #f5f5f5;
+        color:#222222;
+        font: 14px/26px PingFang SC,Hiragino Sans GB,Microsoft YaHei,WenQuanYi Micro Hei,Helvetica Neue,Arial,sans-serif;
+    }
+    a {
+        color: #333;
+        text-decoration: none;
+        cursor: pointer;
     }
     a:hover{ 
         color:#f24e4e;
     }
+    p {
+        display: block;
+        margin-block-start: 1em;
+        margin-block-end: 1em;
+        margin-inline-start: 0px;
+        margin-inline-end: 0px;
+        -webkit-margin-before:0px;
+    }
     .n-main {
-        width: 1400px;
         display: block;
         margin: 0 auto;
     }
@@ -160,17 +184,6 @@ export default {
         display: none;
         padding-top: 12px;
     }
-    .n-title-logo {
-        position: fixed;
-        display: block;
-        float: left;
-        left: 45px;
-        cursor: pointer;
-    }
-    .n-title-logo img {
-        width: 140px;
-        height: 55px;
-    }
     .n-title-ul li {
         margin:4px;
         padding: 0 5px 0 5px;
@@ -185,23 +198,39 @@ export default {
         text-decoration: none;
     }
     .n-middle {
-        margin-left: 200px;
-        display: flex;
-        flex-direction: row;
+        width: 1280px;
+        margin-left: 188px;
+        float:left;
     }
-    .n-left {
+    .n_left {
+        width: 130px;
+        height: auto;
+        position: absolute;
+        top: 0;
+        left: 222px;
+        z-index: 999999;
+    }
+    .n_left_inner {
         position: fixed;
-        left: 50px;
-        top:20px;
-        z-index: 100;
-        background: #eee;
+        top: 0 !important;
+        width: 130px;
+    }
+    .n_title_logo {
+        display: block;
+        margin-left: -8px;
+        cursor: pointer;
+    }
+    .n_title_logo img {
+        width: 140px;
+        height: 55px;
     }
     .n-left-ul {
-         list-style-type: none;
-         font-weight: 500;
-         text-align: center;
-         background: #fff;
-         padding-top: 10px;
+        list-style-type: none;
+        font-weight: 500;
+        text-align: center;
+        background: #fff;
+        padding-top: 10px;
+        border-radius: 8px;
     }
     .n-left-ul li {
         width:130px;
@@ -223,100 +252,144 @@ export default {
         color:#333;
     }
     .n-content {
-        width: 750px;
-        margin: 10px;
-        padding: 0px 10px 0px 10px;
+        width: 690px;
+        margin: 10px 15px 10px 180px;
+        padding: 10px;
         display: flex;
         flex-direction: column;
+        background-color: #fff;
+        float: left;
     }
     .n-content p {
         font-size: 8px;
     }
     .n-content-item {
         width: 100%;
-        display: block;
-        position: relative;
-        margin-top: 2px;
-        text-align: left;
+        overflow: hidden;
+        zoom: 1;
+        margin-bottom: 10px;
         padding-bottom: 10px;
-        padding-top: 10px;
         border-bottom: 1px solid #eee;
     }
-    .n-content-item h3 {
-        cursor: pointer;
-    }
-    .n-content-item-list {
-        display: flex;
-        flex-direction: row;
-        margin: 10px 0 10px 0;
-    }
-    .n-content-item-list img {
-        width: 156px;
+    .n_content_image {
+        display: block;
+        background-color: #f1f1f1;
+        overflow: hidden;
+        width: 154px;
         height: 88px;
-        margin: 0px 4px 0px 4px;
-        cursor: pointer;
+        float: left;
+        margin-right: 15px;
     }
-    .n-conten-show-more {
-        width: 156px;
-        height: 52px;
-        padding-top: 36px;
-        margin: 0px 2px 0px 2px;
-        background-color: #f5f5f5;
-        color: #95bade;
-        cursor: pointer;
-        text-align: center;
+    .n_content_image a,.n_content_image img {
+        display: block;
+        background-color: #f1f1f1;
+        overflow: hidden;
+        width: 154px;
+        height: 88px;
     }
-    .n-content-item p {
-        size: 12px;
-        color: #999;
-        cursor: pointer;
+    .n_content_image img {
+        vertical-align: middle;
+        width: 154px;
+        height: auto;
     }
-    .n-right {
-        display: flex;
-        flex-direction: column;
+    .n_content_desc {
+        width: 495px;
+        height: 88px;
+        float: left;
+        position: relative;
+    }
+    .n_content_desc_inner {
+        display: block;
+        position: absolute;
+        top: 50%;
+        width: 100%;
+        transform: (0,-50%);
+        -webkit-transform: translate(0,-50%);
+        -ms-transform: translate(0,-50%);
+    }
+    .n_content_desc_inner h2 {
+        font-size: 20px;
+        line-height: 1.3;
+        margin-bottom: 4px;
+        font-weight: 700;
+        max-height: 52px;
+        display: -webkit-box;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        margin-block-start: 0.83em;
+        margin-block-end: 0.83em;
+        margin-inline-start: 0px;
+        margin-inline-end: 0px;
+        cursor: pointer;
+        text-align: left;
+    }
+    .n_content_info {
+        font-size: 12px;
+        color: #bbb;
+        margin-top: 4px;
+        text-align: left;
+    }
+    .n_content_info a {
+        font-style: normal;
+        border: 1px solid #eee;
+        border-radius: 3px;
+        padding: 2px 6px;
+        color: #a0a0a0;
+    }
+    .n_right {
+        width: 336px;
+        float: right;
+        margin-right: 35px;
         margin-top:10px;
     }
-    .n-right-list {
-        width: 350px;
-        text-align: left;
-        list-style-type: none;
-        border-top: 5px solid #ff0000;
-        margin: 0 auto;
-        padding-left: 0;
-        margin-left: 0;
-        font-size: 15px;
+    .n_right_container {
+        background-color: #fff;
+        padding: 0 10px 10px 10px;
     }
-    .n-right-list li {
-        margin: 10px 0 10px 0;
-        display: flex;
-        flex-direction: row;
-        flex-wrap: nowrap;
-        align-items: center;
-    }
-    #pic img{
-        width: 118px;
-        height: 66px;
-        margin-right: 5px;
-        cursor: pointer;
-    }
-    .n-right-list-news-title {
-        width:220px;
-        height: 68px;
-        word-break:break-all;
-        font-size: 14px;
-        cursor: pointer;
-    }
-    .n-right-list-news-title a {
-        cursor: pointer;
-    }
-    .n-right-list-news-author{
+    .n_right_today {
+        font-size: 16px;
+        font-weight: bold;
+        padding-left: 20px;
         display: block;
+        line-height: 40px;
+        text-align: left;
+        border-bottom: 1px solid #eee;
+    }
+    .n_right_list {
+        list-style: none;
+        font-size: 14px;
+        margin-block-start: 1em;
+        margin-inline-start: 0px;
+        margin-inline-end: 0px;
+    }
+    .n_right_list li {
+        overflow: hidden;
+        zoom: 1;
+    }
+    .n_right_list li a.image {
+        float: left;
+        width: 100px;
+        height: 57px;
+        display: block;
+        overflow: hidden;
+        margin-right: 10px;
+        background: #f1f1f1;
         position: relative;
-        bottom: 0px;
-        font-size: 12px;
-        color: #9b9597;
         cursor: pointer;
     }
+    .n_right_list li .image img {
+        vertical-align: middle;
+        transition: transform .5s ease-in;
+    }
+    .n_right_list li p {
+        width: 206px;
+        height: 54px;
+        text-align: left;
+        float: left;
+        overflow: hidden;
+        line-height: 25px;
+    }
+    
     .inactive:hover {
         background-color: #fff;
         color: #ff0000;
